7555c2493fc4a48cf0da4bcd2d86d2d09d81c590
Porrith Suong <psuong95@gmail.com>
2016-02-13 18:00:12 -0500

    Adds fixes to type changes.

---
+++ Assets/Scripts/UI/DragImage.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragImage : MonoBehaviour {
+
+    public Vector3 getOriginalPosition { get { return originalPosition; } }
+
+    private Vector3 originalPosition;
+
+	// Use this for initialization
+	void Start () {
+	    originalPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnDrag() {
+        transform.position = Input.mousePosition;
+    }    
+}
