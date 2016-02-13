c019ddb204ab33cfb2240d63e19fc5e925932f8c
Porrith Suong <psuong95@gmail.com>
2016-02-13 14:03:59 -0500

    Adds barebone game state manager.

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum GameStates {isRunning, isPaused}
+
+public class GameManager : MonoBehaviour {
+    
+    public int timeToWait = 5;
+
+    private GameStates gameState;
+
+    private void OnAwake() {
+        gameState = GameStates.isPaused;
+    }
+
+	// Use this for initialization
+	void Start () {
+	    if (gameState != GameStates.isPaused) {
+            gameState = GameStates.isPaused;       
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private IEnumerator PauseGameForSetTime() {
+        yield return new WaitForSeconds(timeToWait);
+    }
+}
--- Assets/Scripts/Managers/TimeKeeper.cs
+++ Assets/Scripts/Managers/TimeKeeper.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameManager : MonoBehaviour {
+public class TimeKeeper : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
