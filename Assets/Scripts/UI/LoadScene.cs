e140dc9f7ba7831f0e6372c3d2c5f089e9fb3427
Porrith Suong <psuong95@gmail.com>
2016-02-13 13:46:06 -0500

    Adds method to load custom scene based on buildIndex.

--- Assets/Scripts/UI/LoadScene.cs
+++ Assets/Scripts/UI/LoadScene.cs
@@ -1,20 +1,8 @@
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class LoadScene : MonoBehaviour {
-
-    
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
     /// <summary>
     /// Get the current active scene and and increment it to load the next scene.
@@ -22,5 +10,12 @@ public class LoadScene : MonoBehaviour {
     public void LoadNextLevel() {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex++);
+    }
+    
+    /// <summary>
+    /// Allows buttons to be assigned to load a specific level.
+    /// </summary>
+    public void LoadCustomScene(int sceneIndex) {
+        SceneManager.LoadScene(sceneIndex);
     }
 }
