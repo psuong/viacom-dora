8cdc8894766452a170a569701774c9c69eac8df6
Porrith Suong <psuong95@gmail.com>
2016-02-14 05:44:42 -0500

    Adds method to disable inactive grids.

--- Assets/Scripts/Managers/GridGenerator.cs
+++ Assets/Scripts/Managers/GridGenerator.cs
@@ -4,7 +4,7 @@ using System.Collections;
 
 public class GridGenerator : MonoBehaviour {
 
-    public GameObject gridPanel;
+    public GameObject[] gridPanels;
 
     private FindList listManager;
 
@@ -14,8 +14,9 @@ public class GridGenerator : MonoBehaviour {
 	private void Start () {
         listManager = FindObjectOfType<FindList>();
         gridObjects = GameObject.FindGameObjectsWithTag("Grid");
-        Invoke("GeneratePanels", 1f);
-	}
+        GeneratePanels();
+        DisableInactiveGrids();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -24,12 +25,25 @@ public class GridGenerator : MonoBehaviour {
 
     private void GeneratePanels() {
         for (int i = 0; i < listManager.accessSprites.Count; i++) {
-            Debug.Log(listManager.accessSprites[i].name);
+            GridSetUp(gridPanels[i], listManager.accessSprites[i]);
         }
     }
 
     private void GridSetUp(GameObject gridObject, GameObject spriteObject) {
-        gridObject.GetComponentInChildren<Image>().sprite = spriteObject.GetComponent<Image>().sprite;
-        Debug.Log(gridObject.name + " " + spriteObject.name);
+        for (int i = 0; i < gridObject.GetComponentsInChildren<Image>().Length; i++) {
+            if (gridObject.GetComponentsInChildren<Image>()[i].tag == "Sprite") {
+                gridObject.GetComponentsInChildren<Image>()[i].sprite = spriteObject.GetComponent<Image>().sprite;
+            }
+        }
+    }
+
+    private void DisableInactiveGrids() {
+        for (int i = 0; i < gridObjects.Length; i++) {
+            for (int j = 0; j < gridObjects[i].GetComponentsInChildren<Image>().Length; j++) {
+                if (gridObjects[i].GetComponentsInChildren<Image>()[j].sprite == null) {
+                    gridObjects[i].SetActive(false);
+                }
+            }
+        }
     }
 }
