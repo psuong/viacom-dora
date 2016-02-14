0621e36747e2fb0f2cef5defcc738ce8663a2e29
s1cyan <claudiab.yan@gmail.com>
2016-02-14 10:47:19 -0500

    Almost complete. Small changes need to be made

---
+++ Assets/Scripts/UI/HighScore.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UIFramework.Singleton;
+
+public class HighScore : UIScreen {
+	private gameTimer score;
+	public string scorebox;
+	string endtime;
+
+	// Use this for initialization
+	void Start () {
+		score = FindObjectOfType<gameTimer> ();
+		endtime = score.secondsString;
+		SetText (scorebox, "You took " + endtime + " seconds to find the items!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
