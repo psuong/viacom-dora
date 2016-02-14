956cb14aaf5610fedf82849410706d2d733e4643
s1cyan <claudiab.yan@gmail.com>
2016-02-13 23:59:12 -0500

    finding stuff works

---
+++ Assets/Scripts/UI/ClickLogic.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ClickLogic : MonoBehaviour {
+	bool foundAll = false;
+	private FindList count;
+
+	// Use this for initialization
+	void Start(){
+		count = GameObject.FindObjectOfType<FindList> ();
+	}
+
+
+	public void findCheck(){
+		if (gameObject.tag == "find"){
+			count.foundCount += 1;
+			print (count);
+			gameObject.GetComponent<Button> ().interactable = false;
+
+		}
+		if (count.foundCount == 5){
+			foundAll = true;
+			Debug.Log ("DONE");
+}
+	}
+}
