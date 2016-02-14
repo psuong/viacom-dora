ddc05ff3140a6a9fa4f823ce5297f489144bc2ea
s1cyan <claudiab.yan@gmail.com>
2016-02-14 03:07:35 -0500

    recording timer works

---
+++ Assets/Scripts/UI/RecordingBar.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RecordingBar : UIScreen {
+	public string timeBar;
+	bool pressed = false;
+	float barFill= 0f;
+	float recordTime = 5f;
+
+
+	public void buttonPresssed(){
+		pressed = true;
+	}
+	void Update(){
+		if (pressed == true) {
+			recordTime -= Time.deltaTime;
+			float barFill = (5 - recordTime)/5;
+			SetImageFill (timeBar, barFill);
+		}
+	}
+}
