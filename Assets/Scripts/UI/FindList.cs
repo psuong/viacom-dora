bb75bd8142402b7df2a59f3345ee337e4c0db835
Porrith Suong <psuong95@gmail.com>
2016-02-14 09:19:05 -0500

    Adds list of items to UI for player to find.

--- Assets/Scripts/UI/FindList.cs
+++ Assets/Scripts/UI/FindList.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections.Generic;
 using UIFramework.Singleton;
 
@@ -6,13 +7,16 @@ public class FindList : Singleton<FindList> {
     
 	public int foundCount = 0;
     public GameObject continueButton;
+    public GameObject itemTextGroup;
 
     public List<GameObject> accessSprites { get { return sprites; } }
 
     private List<GameObject> sprites;
+    private Text[] textItems;
 
     protected override void OnAwake(){
 		sprites = new List<GameObject> ();
+        textItems = itemTextGroup.GetComponentsInChildren<Text>();
 	}
 	// Use this for initialization
 	void Start () {
@@ -21,6 +25,7 @@ public class FindList : Singleton<FindList> {
 		while (listCount < 5){
 			int find = Random.Range (0, sprites.Count);
 			if (sprites[find].gameObject.tag != "find") {
+                textItems[listCount].text = sprites[find].gameObject.name;
 				sprites[find].gameObject.tag = "find";
 				listCount += 1;
 				print (sprites [find]);
