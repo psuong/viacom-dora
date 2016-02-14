21148ba3d7ae808ae1eb2ef7d907d8fee0d8b5d9
s1cyan <claudiab.yan@gmail.com>
2016-02-14 07:46:22 -0500

    audio play for buttons

---
+++ Assets/Scripts/UI/PlaySpanish.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySpanish : MonoBehaviour {
+	AudioSource spanish;
+
+	// Use this for initialization
+	void Start () {
+		spanish = GetComponent<AudioSource> ();
+	
+	}
+	public void playSpanish(){
+		spanish.Play ();
+	}
+
+}
